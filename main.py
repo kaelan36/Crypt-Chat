1684eb8c59d114a894444cfc4d159756cc5d1cf5
kaelan36 <kaelan36@live.ca>
2020-02-29 20:02:53 -0500

    Finished restructuring of main

--- main.py
+++ main.py
@@ -1,136 +1,169 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from crypt_module import *
-import socket, threading, sys, rsa, pickle, json
+import socket, threading, sys, rsa, pickle, json, os
 
-buffSize = 1024
-username = 'User'
+# Class of the Main Window that will be used for display
+class uiMainWindow(object):
+    def __init__(self, MainWindow):
+        self.MainWindow = MainWindow
+        MainWindow.setObjectName('MainWindow')
+        self.mainMenu()
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.enterBtn = QtWidgets.QPushButton(self.centralwidget)
+    def mainMenu(self):
+        MainWindow.resize(300, 200)
+
+        self.centralWidget = QtWidgets.QWidget(self.MainWindow)
+        self.centralWidget.setObjectName('centralWidget')
+
+        self.title = QtWidgets.QLabel(self.centralWidget)
+        self.title.setGeometry(QtCore.QRect(75, 10, 150, 25))
+        self.title.setObjectName('title')
+        self.title.setText('Welcome to Crypt-Chat')
+
+        self.joinBtn = QtWidgets.QPushButton(self.centralWidget)
+        self.joinBtn.setGeometry(QtCore.QRect(75, 45, 150, 25))
+        self.joinBtn.setObjectName('joinBtn')
+        self.joinBtn.setText('Join room')
+        self.joinBtn.clicked.connect(self.chatRoom)
+
+        self.createBtn = QtWidgets.QPushButton(self.centralWidget)
+        self.createBtn.setGeometry(QtCore.QRect(75, 80, 150, 25))
+        self.createBtn.setObjectName('createBtn')
+        self.createBtn.setText('Create a server')
+        #self.createBtn.clicked.connect()
+
+        self.profileBtn = QtWidgets.QPushButton(self.centralWidget)
+        self.profileBtn.setGeometry(QtCore.QRect(75, 115, 150, 25))
+        self.profileBtn.setObjectName('profileBtn')
+        self.profileBtn.setText('View Profile')
+        #self.profileBtn.clicked.connect()
+
+        self.exitBtn = QtWidgets.QPushButton(self.centralWidget)
+        self.exitBtn.setGeometry(QtCore.QRect(75, 150, 150, 25))
+        self.exitBtn.setObjectName('exitBtn')
+        self.exitBtn.setText('Exit')
+        self.exitBtn.clicked.connect(self.exit)
+
+        self.MainWindow.setCentralWidget(self.centralWidget)
+
+    def chatRoom(self):
+
+        # loads keys and info for chat room
+        self.seed, self.pub, self.priv, self.buffSize = loadSeed(self.getPath())
+        self.server = connect(self, self.seed['address'], self.seed['port'], self.seed['name'], self.priv)
+
+        self.MainWindow.resize(800, 600)
+        if self.centralWidget != None:
+            self.centralWidget.destroy()
+
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName('centralWidget')
+
+        self.enterBtn = QtWidgets.QPushButton(self.centralWidget)
         self.enterBtn.setGeometry(QtCore.QRect(210, 460, 75, 23))
         self.enterBtn.setObjectName("enterBtn")
-        self.enterBtn.clicked.connect(self.getMsg)
-        self.output = QtWidgets.QTextEdit(self.centralwidget)
+        self.enterBtn.setText('Send')
+        self.enterBtn.clicked.connect(self.sendMsg)
+
+        self.output = QtWidgets.QTextEdit(self.centralWidget)
         self.output.setGeometry(QtCore.QRect(80, 30, 641, 381))
         self.output.setObjectName("output")
         self.output.setReadOnly(True)
-        self.msgIn = QtWidgets.QLineEdit(self.centralwidget)
+
+        self.msgIn = QtWidgets.QLineEdit(self.centralWidget)
         self.msgIn.setGeometry(QtCore.QRect(290, 460, 241, 20))
         self.msgIn.setObjectName("msgIn")
-        MainWindow.setCentralWidget(self.centralwidget)
 
-        # menu bar
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName("menubar")
-        self.menubar.setNativeMenuBar(False)
-        MainWindow.setMenuBar(self.menubar)
+        MainWindow.setCentralWidget(self.centralWidget)
 
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+    def createProfile():
+        pass
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Crypt Chat"))
-        self.enterBtn.setText(_translate("MainWindow", "Send"))
-
-    def changeTitle(self, MainWindow, title):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", title))
-
-    def getMsg(self):
-
+    def sendMsg(self):
         msg = self.msgIn.text()
-
         if msg == '!quit!':
             server.close()
-            quit()
-
+            self.mainMenu()
         else:
             threading.Thread(target=send, args=(self, msg,)).start()
 
-    def getPath(self, MainWindow):
-        path = QtWidgets.QFileDialog.getOpenFileName(MainWindow, 'Load', './', "JSON Files (*.json)", '')
+    def getPath(self):
+        path = QtWidgets.QFileDialog.getOpenFileName(self.MainWindow, 'Load', './', 'JSON Files (*.json)', '')
         return path[0]
 
-    def getUsername(self, MainWindow):
-        name, pressed = QtWidgets.QInputDialog.getText(MainWindow, 'Set username', 'Enter username:', QtWidgets.QLineEdit.Normal, '')
+    def getUsername(self):
+        name, pressed = QtWidgets.QInputDialog.getText(self.MainWindow, 'Set username', 'Enter username:', QtWidgets.QLineEdit.Normal, '')
         if pressed and name != '':
             return name
 
-def receive(ui, server):
-    while True:
-        try:
-            # receives data and separates actual message from the user address
-            msg = server.recv(buffSize)
-            msg = pickle.loads(msg)
-            msgString = decryptMsg(msg[0], priv)
-            sender = str(msg[1])
-            msg = '<%s> %s' % (sender, msgString)
-            ui.output.append(msg)
-        except OSError as e:
-            print(e)
-            break
+    def changeTitle(self, title):
+        _translate = QtCore.QCoreApplication.translate
+        self.MainWindow.setWindowTitle(_translate('MainWindow', title))
 
-def send(ui, msg, event=None):
-    ui.output.append('<Me> '+msg)
-    msg = encryptMsg(msg, pub)
-    server.send(msg)
+    def exit(self):
+        quit()
 
-# loads server with keys using file
+# Loads a server seed from a given path; file should be JSON
 def loadSeed(path):
 
     # values for building keys
-    keyVals = {}
+    vals = {}
 
-    # info for connecting to server
-    serverInfo = {}
+    # info for connection
+    info = {}
 
     with open(path, 'r') as file:
         data = json.load(file)
-        keyVals = data['key']
-        serverInfo = data['seed']
+        vals = data['key']
+        info = data['seed']
 
-    pub = rsa.PublicKey(keyVals['n'], keyVals['e'])
-    priv = rsa.PrivateKey(keyVals['n'], keyVals['e'], keyVals['d'], keyVals['p'], keyVals['q'])
-    buffer_size = serverInfo['buffer_size']
+    # builds keys and gets buffer size
+    pub = rsa.PublicKey(vals['n'], vals['e'])
+    priv = rsa.PrivateKey(vals['n'], vals['e'], vals['d'], vals['p'], vals['q'])
+    buffer_size = info['buffer_size']
 
-    return serverInfo, pub, priv, buffer_size
+    return info, pub, priv, buffer_size
 
-# Connects to server
-def connect(address, port, ui, name):
+def receive(ui, server, priv):
+    while True:
+        try:
+            # receives data and separates message from sender's name
+            msg = server.recv(ui.buffSize)
+            msg = pickle.loads(msg)
+            sender = str(msg[1])
+            msg = decryptMsg(msg[0], priv)
+            msg = '<%s> %s' % (sender, msg)
+            ui.output.append(msg)
 
-    # gets username
-    username = ui.getUsername(MainWindow)
+        except OSError as e:
+            print(e)
+            ui.mainMenu()
+            break
 
+# sends message to server
+def send(ui, msg):
+    ui.output.append('<Me> '+msg)
+    msg = encryptMsg(msg, ui.pub)
+    ui.server.send(msg)
+
+# Connects to a server
+def connect(ui, address, port, name, priv):
+
+    # gets the username, default will just be user for now
+    username = ui.getUsername()
+
+    # opens socket and connects to server
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.connect((address, port))
     server.send(bytes(username, 'utf8'))
-    ui.changeTitle(MainWindow, name)
 
-    threading.Thread(target=receive, args=(ui, server,)).start()
+    # opens thread to receive messages from server
+    threading.Thread(target=receive, args=(ui, server, priv,)).start()
     return server
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
-ui = Ui_MainWindow()
-ui.setupUi(MainWindow)
-
-# loads all the seed data, separates key values to make keys and the server info becomes the 'seed' on its own
-seed, pub, priv, buffSize = loadSeed(ui.getPath(MainWindow))
-
-server = connect(seed['address'], seed['port'], ui, seed['name'])
+ui = uiMainWindow(MainWindow)
 
 MainWindow.show()
 sys.exit(app.exec_())
-
-server.close()
