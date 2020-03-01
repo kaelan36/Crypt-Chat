0bbe4c080e49e5116abf89e1563659d80fef9f4b
kaelan36 <kaelan36@live.ca>
2020-02-29 22:22:35 -0500

    Prompts finished, working on generation

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class uiMainWindow(object):
     def __init__(self, MainWindow):
         self.MainWindow = MainWindow
         MainWindow.setObjectName('MainWindow')
+        self.changeTitle('Crypt-Chat')
         self.mainMenu()
 
     def mainMenu(self):
@@ -30,7 +31,7 @@ class uiMainWindow(object):
         self.createBtn.setGeometry(QtCore.QRect(75, 80, 150, 25))
         self.createBtn.setObjectName('createBtn')
         self.createBtn.setText('Create a server')
-        #self.createBtn.clicked.connect()
+        self.createBtn.clicked.connect(self.createSeed)
 
         self.profileBtn = QtWidgets.QPushButton(self.centralWidget)
         self.profileBtn.setGeometry(QtCore.QRect(75, 115, 150, 25))
@@ -53,10 +54,9 @@ class uiMainWindow(object):
         self.server = connect(self, self.seed['address'], self.seed['port'], self.seed['name'], self.priv)
 
         self.MainWindow.resize(800, 600)
-        if self.centralWidget != None:
-            self.centralWidget.destroy()
 
-        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        # resets central widget
+        self.centralWidget = QtWidgets.QWidget(self.MainWindow)
         self.centralWidget.setObjectName('centralWidget')
 
         self.enterBtn = QtWidgets.QPushButton(self.centralWidget)
@@ -76,7 +76,80 @@ class uiMainWindow(object):
 
         MainWindow.setCentralWidget(self.centralWidget)
 
-    def createProfile():
+    def createSeed(self):
+
+        self.MainWindow.resize(400, 300)
+
+        # resets central widget
+        self.centralWidget = QtWidgets.QWidget(self.MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+
+        self.title = QtWidgets.QLabel(self.centralWidget)
+        self.title.setObjectName('title')
+        self.title.setText('Fill out prompts to generate seed')
+        self.title.setGeometry(QtCore.QRect(10, 10, 400, 40))
+        self.title.setFont(QtGui.QFont('Verdana', 18, QtGui.QFont.Bold))
+
+        self.nameLbl = QtWidgets.QLabel(self.centralWidget)
+        self.nameLbl.setObjectName('nameLbl')
+        self.nameLbl.setText('Enter a name for your server:')
+        self.nameLbl.setGeometry(QtCore.QRect(10, 70, 174, 25))
+        self.nameIn = QtWidgets.QLineEdit(self.centralWidget)
+        self.nameIn.setObjectName('nameIn')
+        self.nameIn.setGeometry(QtCore.QRect(190, 70, 150, 25))
+
+        self.addrLbl = QtWidgets.QLabel(self.centralWidget)
+        self.addrLbl.setObjectName('nameLbl')
+        self.addrLbl.setText('Enter IP address to bind to:')
+        self.addrLbl.setGeometry(QtCore.QRect(21, 105, 163, 25))
+        self.addrIn = QtWidgets.QLineEdit(self.centralWidget)
+        self.addrIn.setObjectName('addrIn')
+        self.addrIn.setGeometry(QtCore.QRect(190, 105, 150, 25))
+
+        self.portLbl = QtWidgets.QLabel(self.centralWidget)
+        self.portLbl.setObjectName('portLbl')
+        self.portLbl.setText('Enter port to bind to:')
+        self.portLbl.setGeometry(QtCore.QRect(59, 140, 125, 25))
+        self.portIn = QtWidgets.QLineEdit(self.centralWidget)
+        self.portIn.setObjectName('portIn')
+        self.portIn.setGeometry(QtCore.QRect(190, 140, 150, 25))
+        self.onlyInt = QtGui.QIntValidator()
+        self.portIn.setValidator(self.onlyInt)
+
+        self.buffLbl = QtWidgets.QLabel(self.centralWidget)
+        self.buffLbl.setObjectName('buffLbl')
+        self.buffLbl.setText('Enter desired buffer size:')
+        self.buffLbl.setGeometry(QtCore.QRect(33, 175, 151, 25))
+        self.buffIn = QtWidgets.QLineEdit(self.centralWidget)
+        self.buffIn.setObjectName('buffIn')
+        self.buffIn.setGeometry(QtCore.QRect(190, 175, 150, 25))
+        self.buffIn.setValidator(self.onlyInt)
+
+        self.maxLbl = QtWidgets.QLabel(self.centralWidget)
+        self.maxLbl.setObjectName('maxLbl')
+        self.maxLbl.setText('Enter maximum # of users:')
+        self.maxLbl.setGeometry(QtCore.QRect(23, 210, 161, 25))
+        self.maxIn = QtWidgets.QLineEdit(self.centralWidget)
+        self.maxIn.setObjectName('maxIn')
+        self.maxIn.setGeometry(QtCore.QRect(190, 210, 150, 25))
+        self.maxIn.setValidator(self.onlyInt)
+
+        self.createBtn = QtWidgets.QPushButton(self.centralWidget)
+        self.createBtn.setObjectName('createBtn')
+        self.createBtn.setText('Generate')
+        self.createBtn.setGeometry(QtCore.QRect(245, 245, 100, 25))
+        #self.createBtn.clicked.connect()
+
+        self.backBtn = QtWidgets.QPushButton(self.centralWidget)
+        self.backBtn.setObjectName('backbtn')
+        self.backBtn.setText('Back')
+        self.backBtn.setGeometry(QtCore.QRect(10, 245, 75, 25))
+        self.backBtn.clicked.connect(self.mainMenu)
+
+        self.MainWindow.setCentralWidget(self.centralWidget)
+
+
+    def createProfile(self):
         pass
 
     def sendMsg(self):
